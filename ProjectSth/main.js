973e31e3189f316d12917e4faae26a8a5800ec54
codeChill26 <kphuchuynh2005@gmail.com>
2024-10-11 00:05:49 +0700

    Add files via upload

---
+++ ProjectSth/main.js
@@ -0,0 +1,5 @@
+// Function to change the text in the paragraph
+function changeText() {
+    document.getElementById("intro-text").innerHTML = "You clicked the button! The text has changed.";
+    document.getElementById("intro-text").style.color = "#ff6347"; // Change text color
+}
